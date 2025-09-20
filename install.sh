11e98abbc24fb84650e220e0e67df61d33ea6b8b
Danil Samsonov <thrashdev@protonmail.com>
2025-09-20 11:44:57 +0600

    added shit

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo dnf install neovim -y
+sudo dnf install chromium -y
+sudo dnf install alacritty -y
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub net.davidotek.pupgui2
